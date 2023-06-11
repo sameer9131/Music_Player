1f5207b19fba338e61c9ee7b1912d90dd4417ff0
sameer9131 <136237204+sameer9131@users.noreply.github.com>
2023-06-11 17:42:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ var icon=document.getElementById("icon");
 icon.onclick=function(){
    if(mysong.paused){
     mysong.play()
-   icon.src="file:///D:/Web%20Devlopment%20Course/My%20JavaScript%20Project/Music%20Player/pause.jpg"
+   icon.src="pause.jpg"
    setTimeout(() => {
-    icon.src="file:///D:/Web%20Devlopment%20Course/My%20JavaScript%20Project/Music%20Player/play.jpg"
+    icon.src="play.jpg"
    }, 20000);
    }
    else{
     mysong.pause()
-    icon.src="file:///D:/Web%20Devlopment%20Course/My%20JavaScript%20Project/Music%20Player/play.jpg"
+    icon.src="play.jpg"
    }
 }
